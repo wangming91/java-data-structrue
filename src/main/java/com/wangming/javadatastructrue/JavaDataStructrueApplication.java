ad1ae99053aa40538ae532ce4bc55cafa4f3a142
ASYCN\Ming.Wang <1779016007@qq.com>
2019-07-10 16:33:00 +0800

    update

---
+++ src/main/java/com/wangming/javadatastructrue/JavaDataStructrueApplication.java
@@ -0,0 +1,13 @@
+package com.wangming.javadatastructrue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDataStructrueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaDataStructrueApplication.class, args);
+    }
+
+}
